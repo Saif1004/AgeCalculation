20b1f577f898754bb665cbef4e4845ff6634319b
Saif1004 <s.shah7944@student.leedsbeckett.ac.uk>
2023-10-06 18:41:37 +0100

    Initial commit

---
+++ LEC 2.py
@@ -0,0 +1,14 @@
+if __name__ == "__main__":
+    name = input("Hello, Who are you")
+
+    print()
+
+    x=int(input("What year is it today"))
+    y=int(input("What year is it today"))
+
+    z= x-y
+
+    print()
+    print(f"hello,{name}! This year you will be {z} years old")
+
+
